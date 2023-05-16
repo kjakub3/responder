84005ce428fc301df3dc5aa7c96508616af2d343
kjakub3 <106955598+kjakub3@users.noreply.github.com>
2023-05-16 18:56:52 +0200

    RSP-3  Add answers functions (#3)

--- helpers/question-helper.js
+++ helpers/question-helper.js
@@ -4,5 +4,9 @@ module.exports = {
     getQuestionsFromFile: async (fileName) => {
         const fileContent = await readFile(fileName, { encoding: 'utf-8' })
         return JSON.parse(fileContent)
-    }
+    },
+    getQuestion: async (questionId, fileName) => {
+        const questions = await module.exports.getQuestionsFromFile(fileName)
+        return questions?.find(question => question.id === questionId)
+    },
 }
